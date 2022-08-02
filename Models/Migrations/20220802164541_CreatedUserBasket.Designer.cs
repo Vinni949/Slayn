99d0421a70b0498b4f9186a9a052cab91550bfd7
Vinni949 <Pumba0@yandex.ru>
2022-08-02 19:46:12 +0300

    addmigartonsCreatedUsersBasketPrice

--- Models/Migrations/20220802164541_CreatedUserBasket.Designer.cs
+++ Models/Migrations/20220802164541_CreatedUserBasket.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Models1.Model;
 
@@ -11,9 +12,10 @@ using Models1.Model;
 namespace Models1.Migrations
 {
     [DbContext(typeof(DBSlaynTest))]
-    partial class DBSlaynTestModelSnapshot : ModelSnapshot
+    [Migration("20220802164541_CreatedUserBasket")]
+    partial class CreatedUserBasket
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
